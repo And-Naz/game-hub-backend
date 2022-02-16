1337375f259bf683a019e181438f24e42e1eebfb
ANDNAZ <nazaryan-ando@mail.ru>
2022-02-16 15:38:19 +0400

    merge with tets 2

--- routes/About.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-router.get("/", (req, res) => {
-	res.status(200).send({message: 'About Page'})
-})
-
-module.exports = router
--- routes/Games.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-router.get("/", (req, res) => {
-	res.status(200).send({message: 'Games Page'})
-})
-
-module.exports = router
