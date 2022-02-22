7e8de071379085dfa1f64087037df113618f6dd4
And-Naz <nazaryan-ando@mail.ru>
2022-02-22 17:43:58 +0400

    Add games select

--- services/GameService.js
+++ services/GameService.js
@@ -1,12 +1,21 @@
 const { Game } = require('../models')
+const GameDto = require('../dtos/GameDto')
 
 class GameService {
 
 	async all({ limit }) {
+		let queryResult = null
 		if (limit) {
-			return await Game.findAll({ limit: Number(limit) })
+			queryResult = await Game.findAll({ limit: Number(limit) })
+		} else {
+			queryResult = await Game.findAll()
 		}
-		return await Game.findAll()
+		if (!queryResult) { return [] }
+		const dataArr = queryResult.map(res => {
+			const obj = new GameDto({...res.dataValues})
+			return obj
+		})
+		return dataArr
 	}
 }
 
--- services/NewsService.js
+++ services/NewsService.js
@@ -4,10 +4,15 @@ const NewsDto = require('../dtos/NewsDto')
 class NewsService {
 
 	async all() {
-		return await News.findAll({
+		const queryResult = await News.findAll({
 			order: [['date', 'DESC'], ['id', 'DESC']],
 			include: [ Game ]
 		})
+		const dataArr = queryResult.map(res => {
+			const obj = new NewsDto({...res.dataValues, Game: {...res.dataValues.Game.dataValues}})
+			return obj
+		})
+		return dataArr
 	}
 
 	async latest() {
@@ -17,7 +22,7 @@ class NewsService {
 			include: [ Game ]
 		})
 		const dataArr = queryResult.map(res => {
-			const obj = new NewsDto({...res.dataValues, Game: res.dataValues.Game.dataValues})
+			const obj = new NewsDto({...res.dataValues, Game: {...res.dataValues.Game.dataValues}})
 			return obj
 		})
 		return dataArr
